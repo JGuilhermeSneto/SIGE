34d79a99fe9f87bc8ab858e84a631019b09ff62a
José Guilherme da Silva Neto <gu268539@gmail.com>
2026-03-24 09:46:49 -0300

    zekakakkakakak

--- core/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from .models import Aluno  # ou outro model que queira testar
-
-class AlunoModelTest(TestCase):
-    def test_str(self):
-        aluno = Aluno(nome_completo="Teste", cpf="000.000.000-00", data_nascimento="2000-01-01")
-        self.assertEqual(str(aluno), f"{aluno.nome_completo} - {aluno.turma if hasattr(aluno, 'turma') else ''}")
